04ae1a9ef6061d2de5f8489876a0eb89b0f6cb0b
Chulwoo Choi <chulwoochoi@woowahan.com>
2016-03-30 10:44:43 +0900

    logger 작성 slf4j+logback

--- spring-sample-webapp/src/main/java/com/woowa/biz/pilot/SpringSampleApplication.java
+++ spring-sample-webapp/src/main/java/com/woowa/biz/pilot/SpringSampleApplication.java
@@ -1,12 +1,17 @@
 package com.woowa.biz.pilot;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringSampleApplication {
 
+    private static final Logger logger = LoggerFactory.getLogger(SpringSampleApplication.class);
+
     public static void main(String[] args) {
+        logger.info("Starting SpringSampleApplication ...");
         SpringApplication.run(SpringSampleApplication.class, args);
     }
 }
